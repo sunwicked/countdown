9af63b92e86d34d83ff5f53babe3c3e9960791ed
sunny <sunny.aditya@mindtickle.com>
2021-03-08 00:54:22 +0530

    Using Reference https://medium.com/google-developer-experts/compose-oclock-50c778a6360

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,25 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.delay
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +49,114 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Composable
+fun ClockPreview() {
+    fun currentTime(): Time { // 1
+        val cal = Calendar.getInstance()
+        return Time(
+            hours = cal.get(Calendar.HOUR_OF_DAY),
+            minutes = cal.get(Calendar.MINUTE),
+            seconds = cal.get(Calendar.SECOND),
+        )
+    }
+
+    var time by remember { mutableStateOf(currentTime()) } // 2
+    LaunchedEffect(0) { // 3
+        while (true) {
+            time = currentTime()
+            delay(1000)
+        }
+    }
+
+    Clock(time)
+}
+
+
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    Surface {
+        ClockPreview()
+    }
+}
+
+data class Time(val hours: Int, val minutes: Int, val seconds: Int)
+
+@Composable
+fun Clock(time: Time) {
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        val padding = Modifier.padding(horizontal = 4.dp)
+        NumberColumn(0..2, time.hours / 10, padding)
+        NumberColumn(0..9, time.hours % 10, padding)
+        Spacer(Modifier.size(16.dp))
+        NumberColumn(0..5, time.minutes / 10, padding)
+        NumberColumn(0..9, time.minutes % 10, padding)
+        Spacer(Modifier.size(16.dp))
+        NumberColumn(0..5, time.seconds / 10, padding)
+        NumberColumn(0..9, time.seconds % 10, padding)
+    }
+}
+
+
+@Composable
+fun Number(value: Int, active: Boolean, modifier: Modifier) {
+    val backgroundColor by animateColorAsState(
+        if (active) MaterialTheme.colors.primary else MaterialTheme.colors.primaryVariant,
+    )
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .size(40.dp)
+            .background(backgroundColor),
+    ) {
+        Text(
+            text = value.toString(),
+            fontSize = 20.sp,
+            color = Color.White,
+        )
+    }
+}
+
+
+@Composable
+fun NumberColumn(
+    range: IntRange,
+    current: Int,
+    modifier: Modifier = Modifier,
+) {
+    val size = 40.dp
+    val mid = (range.last - range.first) / 2f
+    val reset = current == range.first
+    val offset by animateDpAsState(
+        targetValue = size * (mid - current),
+        animationSpec = if (reset) {
+            spring(
+                dampingRatio = Spring.DampingRatioLowBouncy,
+                stiffness = Spring.StiffnessLow,
+            )
+        } else {
+            tween(
+                durationMillis = 300,
+                easing = LinearOutSlowInEasing,
+            )
+        }
+    )
+
+
+    Column(
+        Modifier
+            .offset(y = offset)
+            .clip(RoundedCornerShape(percent = 25))
+    ) {
+        range.forEach { num ->
+            Number(num, num == current, modifier.size(size))
+        }
     }
 }
 
